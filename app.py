fdcdbd687e3e7c94281ac397c29a31621341a468
gdelgadol <gabs1301@gmail.com>
2025-02-09 11:43:38 -0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from models.products import db
+from controllers.product_controller import *
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:123@172.17.0.2:3306/swarch2024ii_db'
+db.init_app(app)
+app.register_blueprint(product_blueprint)
+
+products = []
+
+@app.route('/')
+def index():
+    return render_template('index.html', products=products)
+
+@app.route('/products', methods=['POST'])
+def add_product():
+    name = request.form['name']
+    description = request.form['description']
+    products.append({'name': name, 'description': description})
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=4200)
